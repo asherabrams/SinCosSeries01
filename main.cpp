165fc4c73f7899bcc74ae3274321dbd4c65dc01d
Asher Abrams <asher813@aol.com>
2019-01-17 13:31:56 -0800

    Update main.cpp
    Renames variables to be more meaningful.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 //  Created by Asher Abrams on 2019/01/07.
 //  Copyright © 2019 | 5779 Asher Abrams. All rights reserved.
 //
-//  Evaluates an approximation of exp(n) and sin(n) using a series.
+//  Evaluates an approximation of exp(x), sin(x), and cos(x) using a series.
 //
 // PREPROCESSOR
 #include <iostream>
@@ -14,43 +14,47 @@
 using namespace std;
 
 // DECLARATIONS
-long int x=1;
-long int y;
+long int term01 = 1;
+long int iterations01;
 double func01(double);
-double z, n, s, c, ssum, csum, sum = 1;
+double invFactorial01, userin01, s, c, ssum, csum, sum = 1;
 
 // DEFINITIONS
-double func01(double x)
+double func01(double factorial01)
 {
-    for(y = 1; y < 20; y++)
+    for(iterations01 = 1; iterations01 < 10; iterations01++)
     {
-        x = x*y;
-        z = double(pow(n,y)/x);
-        sum = sum + z;
-        s = (y%2)*pow(-1,((y/2)%2)) * z;
-        c = ((y+1)%2)*pow(-1,(((y/2)+1)%2)) * z;
+        factorial01 = factorial01*iterations01;
+        invFactorial01 = double(pow(userin01,iterations01)/factorial01);
+        sum = sum + invFactorial01;
+        s = (iterations01%2)*pow(-1,((iterations01/2)%2)) * invFactorial01;
+        c = ((iterations01+1)%2)*pow(-1,(((iterations01/2)+1)%2)) * invFactorial01;
         ssum = ssum + s;
         csum = csum + c;
-        cout << y << '\n';
-        cout << x << '\n';
-        cout << z << '\n';
-        cout << "Exp = " << sum << '\n';
-        cout << "Sin = " << ssum << '\n';
-        cout << "Cos = " << (1 - csum) << '\n';
+        cout << "Iteration " << iterations01 << '\n';
+        cout << "Factorial = " << factorial01 << '\n';
+        cout << "1/Factorial = " << invFactorial01 << '\n';
+        cout << "Exp(x) = " << sum << '\n';
+        cout << "Sin(x) = " << ssum << '\n';
+        cout << "Cos(x) = " << (1 - csum) << '\n';
         cout << '\n';
     }
-    return x;
+    return factorial01;
 }
 
 // MAIN
 int main() {
-    cout << "Evaluates exp(n), sin(n), and cos(n). N?\n";
-    cin >> n;
-    if (double(n))
+    cout << "Evaluates exp(x), sin(x), and cos(x). X?\n";
+    cin >> userin01;
+    if (double(userin01))
     {
-        func01(x);
+        func01(term01);
     }
     else cout << "Error.\n";
     cout << '\n';
     return 0;
 }
+
+
+
+
